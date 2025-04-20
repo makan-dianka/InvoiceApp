f32599d4c664fd0fa980374fbd91c7c953f7a42f
Makan DIANKA <makan.dianka@hotmail.com>
2025-04-20 05:44:12 +0200

    add home and dashboard page

--- factures/urls.py
+++ factures/urls.py
@@ -5,7 +5,6 @@ from .views import auth
 app_name="factures"
 
 urlpatterns = [
-    path("register/", auth.register_view, name="regist"),
-    path("login/", auth.login_view, name="login"),
-    path("logout/", auth.logout_view, name="logout"),
+    path("", auth.home, name="home"),
+    path("dashboard", auth.dashboard, name="dashboard"),
 ]
