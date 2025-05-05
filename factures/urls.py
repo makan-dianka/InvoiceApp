a2a6592b3f480062063261d1663b4375ff2ed7e2
Makan DIANKA <makan.dianka@hotmail.com>
2025-05-05 20:31:34 +0200

    add end slash to urls

--- factures/urls.py
+++ factures/urls.py
@@ -6,8 +6,8 @@ app_name="factures"
 
 urlpatterns = [
     path("", auth.home, name="home"),
-    path("dashboard", auth.dashboard, name="dashboard"),
-    path("invoice/creation", invoice.create, name="create"),
+    path("dashboard/", auth.dashboard, name="dashboard"),
+    path("invoice/creation/", invoice.create, name="create"),
     path("invoice/list", invoice.invoices, name="invoices"),
     path('invoice/<int:invoice_id>/add-item/', invoice.add_item_to_invoice, name='add_item'),
     path('invoice/<int:invoice_id>/detail/', invoice.invoice_detail, name='invoice_detail'),
