d41823ffd5662b874ea5f6efdb0ac0b772f50b57
Makan DIANKA <makan.dianka@hotmail.com>
2025-05-05 20:43:49 +0200

    require user authentication to access dashboard page

--- factures/views/auth.py
+++ factures/views/auth.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
@@ -6,5 +7,7 @@ def home(request):
     return render(request, 'factures/home.html', {})
 
 
+
+@login_required
 def dashboard(request):
     return render(request, 'factures/dashboard.html', {})
