59c595285f45df9dc5f8f557100cbd8bc72e9d00
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-16 17:26:27 +0200

    adapt the Invoice model

--- factures/models/invoice.py
+++ factures/models/invoice.py
@@ -4,35 +4,35 @@ from django.conf import settings
 
 
 class Invoice(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="invoices")
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer')
-    number = models.CharField(max_length=50, unique=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customers')
+    number = models.CharField(max_length=50, unique=True, blank=True)
     issue_date = models.DateField()
-    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
-    title = models.CharField(max_length=250, null=True) # chantier par exemple
-    description = models.TextField(blank=True)
+    chantier = models.CharField(max_length=250, null=True) # chantier par exemple
     is_paid = models.BooleanField(default=False)
-    rg_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)  # Ex: 5.00
-    tva_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)  # Ex: 5.00
+    rg_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=20)  # Ex: 5.00
+    tva_percentage = models.DecimalField(max_digits=5, decimal_places=2, default=20)  # Ex: 5.00
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            super().save(*args, **kwargs)
+
+        if not self.number:
+            self.number = f"FACT-{self.id:04d}"
+            super().save(update_fields=["number"])
+
 
     def total_ht(self):
         return sum(item.total_price for item in self.items.all())
-    
-    def rg_amount(self):
-        return round((self.total_ht() * self.rg_percentage) / 100, 2)
-    
+
     def tva_amount(self):
-        return round((self.total_ht() * self.tva_percentage) / 100, 2)
-    
-    def total_ttc_rg(self):
-        return round(self.total_ht() - self.rg_amount(), 2)
+        return round(self.total_ht() * (self.tva_percentage / 100), 2)
 
     def total_ttc_tva(self):
-        return round(self.total_ht() + self.tva_amount() - self.rg_amount(), 2)
+        return round(self.total_ht() + self.tva_amount(), 2)
 
     def __str__(self):
-        return f"Invoice {self.number} - {self.customer.name}"
+        return self.number
