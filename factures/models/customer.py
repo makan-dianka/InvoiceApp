3ec1d8db77f299bc2538c787395200f64afefbe2
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-16 17:30:45 +0200

    remove related_name because no need to add it

--- factures/models/customer.py
+++ factures/models/customer.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.conf import settings
 
 class Customer(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='customers')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     email = models.EmailField()
     phone = models.CharField(max_length=150)
