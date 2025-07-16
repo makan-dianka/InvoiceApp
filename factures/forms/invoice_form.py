8f6470fda940f7fa6a91447607046771ba58b18c
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-16 17:37:40 +0200

    transform the title field to a construction site field. delete the number field in the form because manage it in the backend. delete me description field because not necessary

--- factures/forms/invoice_form.py
+++ factures/forms/invoice_form.py
@@ -5,33 +5,18 @@ from factures.models.customer import Customer
 class InvoiceForm(forms.ModelForm):
     class Meta:
         model = Invoice
-        fields = ['customer', 'title', 'number', 'issue_date', 'amount', 'description', 'is_paid']
-        
+        fields = ['customer', 'issue_date', 'chantier', 'is_paid']
+
         widgets = {
             'customer': forms.Select(attrs={
                 'class': 'form-select',  # Bootstrap style pour les <select>
             }),
-            'title': forms.TextInput(attrs={
-                'class': 'form-select',
-                'placeholder': 'Titre de la facture'
-            }),
-            'number': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Numéro de facture'
-            }),
+            'chantier': forms.TextInput(attrs={'class': 'form-control'}),
             'issue_date': forms.DateInput(attrs={
                 'class': 'form-control',
                 'type': 'date',
             }),
-            'amount': forms.NumberInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Montant'
-            }),
-            'description': forms.Textarea(attrs={
-                'class': 'form-control',
-                'placeholder': 'Description (optionnel)',
-                'rows': 3
-            }),
+
             'is_paid': forms.CheckboxInput(attrs={
                 'class': 'form-check-input'
             }),
@@ -39,11 +24,8 @@ class InvoiceForm(forms.ModelForm):
 
         labels = {
             'customer': "Client",
-            'title': "Titre",
-            'number': "Numéro de facture",
+            'chantier': "le chantier",
             'issue_date': "Date d'émission",
-            'amount': "Montant",
-            'description': "Description",
             'is_paid': "Payée ?",
         }
 
