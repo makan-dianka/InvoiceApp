7e17442f2c4e5fa2ed3da36e2d6ecfa7ec6b2c30
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-16 17:29:37 +0200

    change the unit field -> user selection only the authorized unit

--- factures/models/invoice_item.py
+++ factures/models/invoice_item.py
@@ -3,9 +3,16 @@ from django.db import models
 
 
 class InvoiceItem(models.Model):
+    UNIT = [
+        ('m2', 'M²'),
+        ('ml', 'ML'),
+        ('u', 'Unité'),
+        ('autre', 'Autre'),
+    ]
+
     invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE, related_name="items")
     description = models.TextField()
-    unit = models.CharField(max_length=10)  # ex: m2, u, ml
+    unit = models.CharField(max_length=10, choices=UNIT, default='m2')  # ex: m2, u, ml
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
     unit_price = models.DecimalField(max_digits=10, decimal_places=2)
 
