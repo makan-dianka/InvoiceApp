1abde8043dd88bf0fc1421e4ed729cff4f630aba
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-17 04:29:13 +0200

    user company admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Company
 
-# Register your models here.
+@admin.register(Company)
+class ComapnyAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'name', 'address', 'email', 'phone', 'siret', 'naf')
+    search_fields = ('name', 'siret')
+    list_filter = ('email',)
