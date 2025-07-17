2779bc85597bfa287ac3541938f8f870c8924cdf
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-17 05:37:53 +0200

    add comapny creation url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import auth
+from .views import auth, company
 
 
 app_name="accounts"
@@ -8,4 +8,5 @@ urlpatterns = [
     path("register/", auth.register_view, name="register"),
     path("login/", auth.login_view, name="login"),
     path("logout/", auth.logout_view, name="logout"),
+    path("companyCreation/", company.creation, name="company_creation"),
 ]
