f4606d7f18edd7d1c6487c8d704642458f7ebb11
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-17 05:40:04 +0200

    company creation views and template

---
+++ accounts/views/company.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from accounts.forms.userform import CompanyForm
+
+
+@login_required
+def creation(request):
+    if request.method == 'POST':
+        form = CompanyForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.owner = request.user
+            instance.save()
+            return redirect('factures:dashboard')
+    form = CompanyForm()
+    context = {'form' : form}
+    return render(request, 'accounts/company_creation.html', context)
