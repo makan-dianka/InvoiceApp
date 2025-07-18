dacd8deaab7674eccc6cc6b7309a30b570ee5c66
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-18 03:39:15 +0200

    render tva_percentage field with form

--- factures/forms/invoice_form.py
+++ factures/forms/invoice_form.py
@@ -5,13 +5,18 @@ from factures.models.customer import Customer
 class InvoiceForm(forms.ModelForm):
     class Meta:
         model = Invoice
-        fields = ['customer', 'issue_date', 'chantier', 'is_paid']
+        fields = ['customer', 'issue_date', 'chantier', 'tva_percentage', 'is_paid']
 
         widgets = {
             'customer': forms.Select(attrs={
                 'class': 'form-select', 'id' : 'customer-field',  # Bootstrap style pour les <select>
             }),
             'chantier': forms.TextInput(attrs={'class': 'form-control'}),
+            'tva_percentage': forms.NumberInput(attrs={
+                'class': 'form-control',
+                'step': '0.01',
+                'min': '0',
+            }),
             'issue_date': forms.DateInput(attrs={
                 'class': 'form-control',
                 'type': 'date',
@@ -24,11 +29,15 @@ class InvoiceForm(forms.ModelForm):
 
         labels = {
             'customer': "Client",
-            'chantier': "le chantier",
+            'chantier': "Le chantier",
+            'tva_percentage' : 'TVA',
             'issue_date': "Date d'émission",
             'is_paid': "Payée ?",
         }
 
+        help_texts = {
+            'tva_percentage': "Indiquez le pourcentage de TVA applicable (exemple : 5 pour 5%). À défault c'est 20%",
+        }
 
 
 
