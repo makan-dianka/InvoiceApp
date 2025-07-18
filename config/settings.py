31cb7beade5b670663cc5d8345a02a1d54c973ea
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-18 19:50:13 +0200

    configure static and media root

--- config/settings.py
+++ config/settings.py
@@ -111,15 +111,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-MEDIA_URL = '/images/'
-
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_URL = '/media/'
+
 STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles'))
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
