78309d976edae7532c73d5f02c84f3ec3996384e
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-22 01:01:39 +0200

    delete these admins in the app accounts - for the payment app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,18 +1,8 @@
 from django.contrib import admin
-from . models import Company, UserAccess, Payment
+from . models import Company
 
 @admin.register(Company)
 class ComapnyAdmin(admin.ModelAdmin):
     list_display = ('owner', 'name', 'address', 'email', 'phone', 'siret', 'naf')
     search_fields = ('name', 'siret')
     list_filter = ('email',)
-
-
-@admin.register(UserAccess)
-class UserAccessAdmin(admin.ModelAdmin):
-    list_display = ('user', 'trial_start', 'trial_end', 'paid_until')
-
-
-@admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'customer_id', 'charge_id', 'amount', 'date')
