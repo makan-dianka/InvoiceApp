ac3597ef6d9065e899f3e91063bb1c32c9df4e78
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-22 00:58:37 +0200

    delete these models in the accounts app - they are added in the payment app

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 from django.db import models
 from django.utils import timezone
 from django.conf import settings
-from datetime import timedelta
+
 
 
 
@@ -36,12 +36,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.email
+        return self.first_name
 
 
 
 class Company(models.Model):
-    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='company', blank=True)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='company', blank=True)
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=250)
     email = models.EmailField(unique=True)
@@ -51,37 +51,3 @@ class Company(models.Model):
 
     def __str__(self):
         return self.name
-
-
-
-
-
-class UserAccess(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='useraccess')
-    trial_start = models.DateTimeField(default=timezone.now)
-    trial_end = models.DateTimeField()
-    paid_until = models.DateTimeField(blank=True, null=True)
-
-    def is_active(self):
-        now = timezone.now()
-        return now <= self.trial_end or (self.paid_until and now <= self.paid_until)
-
-    @classmethod
-    def create_for_user(cls, user):
-        trial_end = timezone.now() + timedelta(days=15)
-        return cls.objects.create(user=user, trial_end=trial_end)
-
-
-    def __str__(self):
-            return f"{self.user} — Actif : {self.is_active()}"
-
-
-class Payment(models.Model):
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    customer_id = models.CharField(max_length=250)
-    charge_id = models.CharField(max_length=250)
-    amount = models.DecimalField(max_digits=6, decimal_places=2)
-    date = models.DateTimeField()
-
-    def __str__(self):
-        return self.customer_id
