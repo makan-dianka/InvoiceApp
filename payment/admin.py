8b3e9b0f57436b39d33c08dbe267a7646a094844
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-22 00:54:59 +0200

    save the UserAccess and Payment model in django admin

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . models import UserAccess, Payment
 
-# Register your models here.
+
+@admin.register(UserAccess)
+class UserAccessAdmin(admin.ModelAdmin):
+    list_display = ('user', 'trial_start', 'trial_end', 'paid_until')
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'customer_id', 'charge_id', 'amount', 'date')
