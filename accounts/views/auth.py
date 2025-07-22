617f8a388ac2233896138e6f165f8cf4b34de37e
Makan DIANKA <makan.dianka@hotmail.com>
2025-07-22 06:24:30 +0200

    send email when registering a user

--- accounts/views/auth.py
+++ accounts/views/auth.py
@@ -3,7 +3,8 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm #UserCreationForm
 from  accounts.forms.userform import CreateUserForm
 from django.contrib import messages
-# from models import EmailBackend
+from django.conf import settings
+from utils.payment import sendmail
 
 
 
@@ -17,6 +18,16 @@ def register_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+
+            # sendmail
+            maildata = {
+            'usermail': settings.EMAIL_RECIPIENT,
+            'mailsubject' : f"[OkFacture] Un nouveau utilisateur vient de s'inscrire",
+            'html_file': 'register.html',
+            'txt_file': 'register.txt',
+            }
+            sendmail(request, **maildata)
+
             return redirect('accounts:company_creation')  # à adapter plus tard
     else:
         form = CreateUserForm()
